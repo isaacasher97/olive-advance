cecd5ebe42ece065e4b05af227328e5ec251dcad
isaacasher97 <isaac.asher97@gmail.com>
2023-12-06 18:26:03 -0500

    added styling to navbar and hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -13,17 +13,17 @@
       <div
         class="p-5 mb-4 lc-block col-xxl-7 col-lg-8 col-12"
         style="
-          backdrop-filter: blur(0px) saturate(102%);
+          backdrop-filter: blur(2px) saturate(102%);
           -webkit-backdrop-filter: blur(6px) saturate(102%);
-          background-color: rgba(255, 255, 255, 0.45);
+          background-color: rgba(0, 0, 0, 0.45);
           border-radius: 12px;
           border: 1px solid rgba(209, 213, 219, 0.3);
         "
       >
         <div class="lc-block">
           <div editable="rich">
-            <h2 class="fw-bolder display-3">
-              Efficient Solutions for Business Funding Needs
+            <h2 class="fw-bolder display-4">
+              An alternative approach to business financing
             </h2>
           </div>
         </div>
@@ -32,14 +32,15 @@
             <p class="lead">
               Explore rapid and straightforward business funding solutions with
               Olive Advance. Our dedication is to simplify the process, making
-              business funding a seamless experience. Receive a personalized
-              quote within minutes, empowering your journey to financial
-              success.
+              business funding an easy, seamless experience. Receive a
+              personalized quote within minutes.
             </p>
           </div>
         </div>
-        <div class="lc-block">
-          <a class="btn btn-dark" href="#" role="button">Apply Now</a>
+        <div id="apply-button" class="lc-block">
+          <button class="bn47">
+            <a href="#" role="button">Apply Now</a>
+          </button>
         </div>
       </div>
     </div>
@@ -58,4 +59,75 @@ export default {
     width: 100%;
   }
 }
+
+h2,
+p {
+  color: #f7f7f7;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+h2,
+a {
+  text-align: center;
+}
+
+button {
+  background-color: #183b54;
+}
+
+/* .btn {
+  display: flex;
+  justify-content: center;
+  width: 50%;
+  margin: 0 auto;
+} */
+
+#apply-button {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: 0 auto;
+}
+
+/* button styling  */
+.bn47 {
+  position: relative;
+  padding: 1rem 2rem 0.5rem 2.5rem;
+  color: #000000;
+  border: 1px dashed #f7f7f7;
+  -webkit-transition: padding 0.3s ease-in-out;
+  transition: padding 0.3s ease-in-out;
+}
+
+.bn47:before {
+  content: "";
+  position: absolute;
+  top: 0.5rem;
+  left: 0.5rem;
+  z-index: -1;
+  height: 100%;
+  width: 100%;
+  background-color: #183b54;
+  border-right: 3px solid #183b54;
+  border-bottom: 3px solid #183b54;
+  -webkit-transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
+}
+
+.bn47:hover {
+  padding: 0.75rem 2.25rem;
+}
+
+.bn47:hover:before {
+  top: 0;
+  left: 0;
+}
+
+a {
+  text-decoration: #183b54;
+  color: #f7f7f7;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 800;
+}
+/* button styling end  */
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,38 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/about">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/contact">Contact Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">APPLY NOW</a>
-          </li>
-        </ul>
+  <div id="nav-container">
+    <nav style="background-color: #183b54" class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Navbar</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">APPLY NOW</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -42,6 +44,15 @@ export default {
 <style scoped>
 .navbar-collapse {
   flex-grow: 0;
+}
+
+a {
+  color: #f7f7f7;
+  font-weight: 800;
+}
+
+.navbar-toggler {
+  background-color: #f7f7f7;
 }
 
 @media screen and (min-width: 1000px) {
