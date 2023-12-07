e172d82d5fe1bcb5583527a7c03ff8edd5c3c991
isaacasher97 <isaac.asher97@gmail.com>
2023-12-07 13:01:15 -0500

    Carousel added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <Hero />
     <InfoSection />
     <HeroTwo />
+    <CardCarousel />
   </div>
 </template>
 
@@ -10,9 +11,10 @@
 import Hero from "../components/Hero.vue";
 import InfoSection from "../components/InfoSection.vue";
 import HeroTwo from "../components/HeroTwo.vue";
+import CardCarousel from "../components/CardCarousel.vue";
 
 export default {
   name: "HomeView",
-  components: { Hero, InfoSection, HeroTwo },
+  components: { Hero, InfoSection, HeroTwo, CardCarousel },
 };
 </script>
