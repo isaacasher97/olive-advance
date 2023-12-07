b6feadcf20eac96714e249237024f4171fd0c95a
isaacasher97 <isaac.asher97@gmail.com>
2023-12-07 11:27:00 -0500

    added second hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,17 @@
   <div class="home">
     <Hero />
     <InfoSection />
+    <HeroTwo />
   </div>
 </template>
 
 <script>
 import Hero from "../components/Hero.vue";
 import InfoSection from "../components/InfoSection.vue";
+import HeroTwo from "../components/HeroTwo.vue";
 
 export default {
   name: "HomeView",
-  components: { Hero, InfoSection },
+  components: { Hero, InfoSection, HeroTwo },
 };
 </script>
