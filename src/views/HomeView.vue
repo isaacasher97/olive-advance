6c3912a34ded24779c080b260846582a6252c971
isaacasher97 <isaac.asher97@gmail.com>
2023-12-06 23:25:18 -0500

    added info component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home">
     <Hero />
+    <InfoSection />
   </div>
 </template>
 
 <script>
 import Hero from "../components/Hero.vue";
+import InfoSection from "../components/InfoSection.vue";
 
 export default {
   name: "HomeView",
-  components: { Hero },
+  components: { Hero, InfoSection },
 };
 </script>
